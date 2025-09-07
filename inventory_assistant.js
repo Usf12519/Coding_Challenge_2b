73fa2ed89ec5cc2f8d3d9e959be6ecf008d91624
Cameron Smith <Cs11@usf.edu>
2025-09-07 01:01:02 -0400

    inventory_assistant.js

---
+++ inventory_assistant.js
@@ -0,0 +1,27 @@
+// Step 1: Create and Initalize Variables
+
+itemName = (computer,'computer');
+unitCost = (2.40);
+currentStock = (250);
+reorderLevel = (150);
+targetStock = ('one month');
+weeklyDemand = (100);
+supplierLeadTimeWeeks = ("2 weeks");
+
+// Step 2: Calculate Inventory Metrics
+
+weeksOfCover = ('100 > 0 , 250 / 100 :  var Infinity');
+stockDefecit = (Math.max('0, one month - 250'));
+reorderQuantity = (250 <= 150 || '100 > 0 , 250 / 100 : var Infinity' < 
+    '2 weeks'?);
+    Math.ceil(Math.max('0, one month - 250')) : 0;
+    estimatedReorderCost = (250 <= 150 || '100 > 0 , 250 / 100 : var Infinity' < 
+    '2 weeks'? * 2.40);
+    reorderNow = (250 <= 150 || '100 > 0 , 250 / 100 :  var Infinity' < 
+        "2 weeks");
+
+// Step 3: Print to Console
+
+console.log(computer,'computer');
+console.log('100 > 0 , 250 / 100 :  var Infinity')
+console.log()
